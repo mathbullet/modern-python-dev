7425d2f64c143506b6bbbf38c157ca63f96db315
mathbullet <mathbullet.compling@gmail.com>
2025-11-22 01:34:10 +0900

    Add type mismatch examples with and without type hints in separate files

---
+++ src/type_mismatch.py
@@ -0,0 +1,5 @@
+def repeat_message(message: str, times: int) -> str:
+    return message * times
+
+res = repeat_message(3, 4)
+print(res)
---
+++ src/type_mismatch_no_hints.py
@@ -0,0 +1,5 @@
+def repeat_message(message, times):
+    return message * times
+
+res = repeat_message(3, 4)
+print(res)
